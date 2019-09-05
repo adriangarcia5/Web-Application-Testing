faf66982f866e52438ea2bd171ea35b9084cce8d
Adrian-Garcia <agarc5@hotmail.com>
2019-09-05 15:12:12 -0500

    set up components & app/functional/ready for tests

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
+
 import './App.css';
 
 function App() {
+
+    const [strikes, setStrikes] = useState(0)
+    const [balls, setBalls] = useState(0)
+    const [fouls, setFouls] = useState(0)
+    const [hits, setHits] = useState(0)
+
+
+    const actions = {
+        strike: () => {
+            setStrikes(strikes +1);
+            if (strikes === 2){
+              return setStrikes(0), setBalls(0);
+            }
+        },
+
+        ball: () => {
+          setBalls(balls + 1);
+          if (balls === 3) {
+            return setBalls(0), setStrikes(0);
+          }
+        },
+
+        foul : () => {
+          if( strikes <= 1) {
+            return setStrikes(strikes + 1)
+          }
+        },
+
+        score: () => {
+          if(setHits(hits + 1)) {
+            return setBalls(0), setStrikes(0), setFouls(0);
+          }
+        }
+    }
+
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <h1>Play Ball!</h1>
+      <Display 
+      strike={strikes}
+      ball={balls}
+      foul={fouls}
+      score={hits}
+      />
+      <Dashboard 
+                play={actions}
+      />
     </div>
   );
 }
